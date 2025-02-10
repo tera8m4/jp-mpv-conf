5cb6647a5239bdf7680538433adfd5e49c95616d
Vedang Patel <pvedang388@gmail.com>
2025-02-11 05:15:45 +0530

    simplified mpv2anki.lua even further

--- scripts/mpv2anki.lua
+++ scripts/mpv2anki.lua
@@ -34,14 +34,10 @@ local msg = require 'mp.msg'
 mp.options = require "mp.options"
 mp.options.read_options(options, "mpv2anki")
 
-local subs = {}
-local enable_subs_to_clip = true
 local debug_mode = false
-local clipboard_var
 
 if unpack ~= nil then table.unpack = unpack end
 
-local o = {}
 local prefix
 if mp.get_property_native("platform") == "windows" then
   prefix = utils.join_path(os.getenv('APPDATA'), [[Anki2\]] .. options.profile_name .. [[\collection.media]])
@@ -55,33 +51,8 @@ local function dlog(...)
   end
 end
 
-local function clean(s)
-  for _, ws in ipairs({'%s', ' ', '᠎', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '​', ' ', ' ', '　', '﻿', '‪'}) do
-    s = s:gsub(ws..'+', "")
-  end
-  return s
-end
-
 local function get_name(s, e)
   return mp.get_property("filename"):gsub('%W','').. tostring(s) .. tostring(e)
-end
-
-
-local function record_sub(_, text)
-  if text and mp.get_property_number('sub-start') and mp.get_property_number('sub-end') then
-    local sub_delay = mp.get_property_native("sub-delay")
-    local audio_delay = mp.get_property_native("audio-delay")
-    local newtext = clean(text)
-    if newtext == '' then
-      return
-    end
-
-    subs[newtext] = { mp.get_property_number('sub-start') + sub_delay - audio_delay, mp.get_property_number('sub-end') + sub_delay - audio_delay }
-    dlog(string.format("%s -> %s : %s", subs[newtext][1], subs[newtext][2], newtext))
-    -- Remove newlines from text
-    text = string.gsub(text, "[\n\r]+", " ")
-    clipboard_var = text
-  end
 end
 
 local function create_audio(s, e)
@@ -96,20 +67,6 @@ local function create_audio(s, e)
   local t = e - s + options.audio_clip_padding
   local source = mp.get_property("path")
   local aid = mp.get_property("aid")
-
-  local tracks_count = mp.get_property_number("track-list/count")
-  for i = 1, tracks_count do
-    local track_type = mp.get_property(string.format("track-list/%d/type", i))
-    local track_selected = mp.get_property(string.format("track-list/%d/selected", i))
-    if track_type == "audio" and track_selected == "yes" then
-      if mp.get_property(string.format("track-list/%d/external-filename", i), o) ~= o then
-        source = mp.get_property(string.format("track-list/%d/external-filename", i))
-        aid = 'auto'
-      end
-      break
-    end
-  end
-
 
   local cmd = {
     'run',
@@ -198,27 +155,15 @@ local function add_to_last_added(ifield, afield, tfield)
 end
 
 local function get_extract()
-  local lines = clipboard_var
-  local e = 0
-  local s = 0
-  for line in lines:gmatch("[^\r\n]+") do
-    line = clean(line)
-    dlog(line)
-    if subs[line]~= nil then
-      if subs[line][1] ~= nil and subs[line][2] ~= nil then
-        if s == 0 then
-          s = subs[line][1]
-        else
-          s = math.min(s, subs[line][1])
-        end
-        e = math.max(e, subs[line][2])
-      end
-    else
-      mp.osd_message("ERR! Line not found: " .. line, 3)
-      return
-    end
-  end
+  local lines = mp.get_property_native("sub-text")
+  dlog(lines)
+
+  local sub_delay = mp.get_property_native("sub-delay")
+  local audio_delay = mp.get_property_native("audio-delay")
+  local s = math.min(mp.get_property_number('sub-start') + sub_delay - audio_delay)
+  local e = math.max(mp.get_property_number('sub-end') + sub_delay - audio_delay)
   dlog(string.format('s=%d, e=%d', s, e))
+
   if e ~= 0 then
     create_screenshot(s, e)
     create_audio(s, e)
@@ -236,27 +181,6 @@ local function ex()
     pcall(get_extract)
   end
 end
-
-local function rec(...)
-  if debug_mode then
-    record_sub(...)
-  else
-    pcall(record_sub, ...)
-  end
-end
-
-
-local function toggle_debug_mode()
-  debug_mode = not debug_mode
-  mp.osd_message("Debug mode " .. (debug_mode and "activated" or "deactived"), 3)
-end
-
-local function clear_subs(_)
-  subs = {}
-end
-
-mp.observe_property("sub-text", 'string', rec)
-mp.observe_property("filename", "string", clear_subs)
 
 mp.add_key_binding("ctrl+a", "update-anki-card", ex)
 mp.add_key_binding("ctrl+A", ex)
